b1a806902078a22bb87c57afd4418da85fbf3c77
douhaku <douhaku1115@gmail.com>
2019-10-03 20:09:37 +0900

    爆破処理修正

--- Source.cpp
+++ Source.cpp
@@ -32,6 +32,7 @@ enum {
 	CELL_TYPE_NONE,
 	CELL_TYPE_HARD_BLOCK,
 	CELL_TYPE_SOFT_BLOCK,
+	CELL_TYPE_EXPLOSION,
 	CELL_TYPE_MAX
 };
 
@@ -39,18 +40,20 @@ int cells[MAP_HEIGHT][MAP_WIDTH];
 char cellAA[][2 + 1] = {
 	"　",//CELL_TYPE_NONE,
 	"■",//CELL_TYPE_HARD_BLOCK,
-	"□"//CELL_TYPE_SOFT_BLOCK,
+	"□",//CELL_TYPE_SOFT_BLOCK,
+	"※"//CELL_TYPE_EXPLOSION,
 };
 typedef struct {
 	int x, y;
 	int direction;
+	bool isDead;
 }MONSTER;
 #define MONSTER_MAX 9
 MONSTER monsters[MONSTER_MAX];
 
 int getMonster(int _x,int _y) {
 	for (int i = 0; i < MONSTER_MAX; i++)
-		if ((monsters[i].x == _x) && (monsters[i].y == _y))
+		if ((!monsters[i].isDead)&&(monsters[i].x == _x) && (monsters[i].y == _y))
 		return i;
 	return -1;
 }
@@ -75,13 +78,21 @@ void setFreePosition(int* pX, int* pY) {
 	}
 
 }
+int getBomb(int _x, int _y) {
+	for (int i = 0; i < BOMB_MAX; i++)
+		if ((bombs[i].count > 0) && (bombs[i].x == _x) && (bombs[i].y == _y))
+			return i;
+	return -1;
+
+}
 bool checkCanMove(int _x,int _y) {
 	switch (cells[_y][_x]) {
 	case CELL_TYPE_HARD_BLOCK:
 	case CELL_TYPE_SOFT_BLOCK:
 		return false;
 	default:
-		if (getMonster(_x, _y) < 0)
+		if ((getMonster(_x, _y) < 0)
+			&& getBomb(_x,_y)<0)
 			return true;
 		return false;
 		break;
@@ -95,20 +106,38 @@ int getFreeBomb() {
 		return -1;
 	}
 }
-int getBomb(int _x,int _y) {
-	for (int i = 0; i < BOMB_MAX; i++) 
-		if ((bombs[i].x == _x) && (bombs[i].y == _y))
-			return i;
-		return -1;
-	
+void explosion(int _x,int _y) {
+	cells[_y][_x] = CELL_TYPE_EXPLOSION; //爆発
+	for (int j = 0; j < DIRECTION_MAX; j++) {//全方向
+		int x = _x;//爆発中心
+		int y = _y;
+		for (int i = 0; i < 2; i++) {
+			x += directions[j][0];//2マス
+			y += directions[j][1];
+			if (cells[y][x] == CELL_TYPE_HARD_BLOCK)
+				break;
+			else if (cells[y][x] == CELL_TYPE_SOFT_BLOCK) {
+				cells[y][x] = CELL_TYPE_EXPLOSION;
+				break;
+			}
+			else {
+				int monster = getMonster(x, y);
+				if (monster > 1) {
+					monsters[monster].isDead = true;
+				}
+				cells[y][x] = CELL_TYPE_EXPLOSION;//爆発
+			}
+		}
+	}
 }
+
 int main() {
 	srand(time(NULL));
-	for (int y = 0; y < MAP_HEIGHT; y+=MAP_HEIGHT-1)
-		for (int x=0;x<MAP_WIDTH;x++)
+	for (int y = 0; y < MAP_HEIGHT; y += MAP_HEIGHT - 1)
+		for (int x = 0; x < MAP_WIDTH; x++)
 			cells[y][x] = CELL_TYPE_HARD_BLOCK;
 	for (int y = 0; y < MAP_HEIGHT; y++)
-		for (int x = 0; x < MAP_WIDTH; x+=MAP_WIDTH-1)
+		for (int x = 0; x < MAP_WIDTH; x += MAP_WIDTH - 1)
 			cells[y][x] = CELL_TYPE_HARD_BLOCK;
 	for (int y = 0; y < MAP_HEIGHT; y++)
 		for (int x = 1; x < MAP_WIDTH - 1; x++) {
@@ -123,11 +152,11 @@ int main() {
 
 	for (int i = 1; i < MONSTER_MAX; i++)
 		setFreePosition(&monsters[i].x, &monsters[i].y);
-	
+
 	while (1) {
 		system("cls");
 		//int left = 0;
-		int left = monsters[0].x - (SCREEN_WIDTH /2);
+		int left = monsters[0].x - (SCREEN_WIDTH / 2);
 		if (left < 0) {
 			left = 0;
 		}
@@ -144,14 +173,20 @@ int main() {
 					printf("＠");
 				else if (bomb >= 0) {
 					char aa[] = "０";
-						aa[1] + bombs[bomb].count;
-						printf(aa);
+
+					aa[1] += bombs[bomb].count;
+					printf(aa);
 				}
 				else
 					printf(cellAA[cells[y][x]]);
 			}
 			printf("\n");
 		}
+		for (int y = 1; y < MAP_HEIGHT - 1; y++)
+			for (int x = 1; x < MAP_WIDTH - 1; x++) {
+				if (cells[y][x] == CELL_TYPE_EXPLOSION)
+					cells[y][x] = CELL_TYPE_NONE;
+			}
 		int x = monsters[0].x;
 		int y = monsters[0].y;
 		switch (_getch()) {
@@ -164,21 +199,34 @@ int main() {
 			if (bomb >= 0) {
 				bombs[bomb].x = monsters[0].x;
 				bombs[bomb].y = monsters[0].y;
+				bombs[bomb].count = BOMB_COUNT_MAX;
 			}break;
 		}
 		if (checkCanMove(x, y)) {
 			monsters[0].x = x;
 			monsters[0].y = y;
 		}
-		for (int i = 1; i < MONSTER_MAX; i++) {
-			int x = monsters[i].x + directions[monsters[i].direction][0];
-			int y = monsters[i].y + directions[monsters[i].direction][1];
-			if (checkCanMove(x, y)) {
-				monsters[i].x = x;
-				monsters[i].y = y;
-			}
-			else
-				monsters[i].direction = rand() % DIRECTION_MAX;
+		for (int i = 0; i < BOMB_MAX; i++) {
+			if (bombs[i].count <= 0)
+				continue;
+			bombs[i].count--;
+			if (bombs[i].count <= 0)
+				//cells[bombs[i].y][bombs[i].x] = CELL_TYPE_EXPLOSION; 
+				explosion(bombs[i].x, bombs[i].y);//爆破
 		}
+		
+			for (int i = 1; i < MONSTER_MAX; i++) {
+				if (monsters[i].isDead)
+					continue;
+				int x = monsters[i].x + directions[monsters[i].direction][0];
+				int y = monsters[i].y + directions[monsters[i].direction][1];
+				if (checkCanMove(x, y)) {
+					monsters[i].x = x;
+					monsters[i].y = y;
+				}
+				else
+					monsters[i].direction = rand() % DIRECTION_MAX;
+			
+			}
 	}
 }
